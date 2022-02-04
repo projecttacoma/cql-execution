cf7e0eacbd4abe8596d6444aee9f82973fc48c78
mgramigna <mgramigna@mitre.org>
2022-02-04 15:43:24 -0500

    interval tests converted (some issues)

--- src/elm/expression.ts
+++ src/elm/expression.ts
@@ -39,7 +39,7 @@ export class Expression {
 
   async execArgs(ctx: Context) {
     if (this.args != null) {
-      // TODO (MATT): check this
+      // TODO (MATT) ALERT: check this. doesn't work at all. sad
       return Promise.all(this.args.map(async arg => arg.execute(ctx)));
     } else if (this.arg != null) {
       return this.arg.execute(ctx);
