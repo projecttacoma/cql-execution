b362daee150c50273bc6f0eae32819d14f4df97b
Chris Hossenlopp <hossenlopp@mitre.org>
2022-02-04 19:52:43 -0500

    fixed should issues in elm external-test

--- test/elm/external/external-test.ts
+++ test/elm/external/external-test.ts
@@ -1,3 +1,4 @@
+import should from 'should';
 import setup from '../../setup';
 const data = require('./data');
 const vsets = require('./valuesets');
@@ -11,7 +12,7 @@ describe('Retrieve', () => {
 
   it('should find conditions', async function () {
     const c = await this.conditions.exec(this.ctx);
-    c.should.have.length(2);
+    should(c).have.length(2);
     c[0].id.should.equal('http://cqframework.org/3/2');
     c[1].id.should.equal('http://cqframework.org/3/4');
     this.ctx.evaluatedRecords.should.have.length(2);
@@ -20,7 +21,7 @@ describe('Retrieve', () => {
 
   it('should find encounter performances', async function () {
     const e = await this.encounters.exec(this.ctx);
-    e.should.have.length(3);
+    should(e).have.length(3);
     e[0].id.should.equal('http://cqframework.org/3/1');
     e[1].id.should.equal('http://cqframework.org/3/3');
     e[2].id.should.equal('http://cqframework.org/3/5');
@@ -30,7 +31,7 @@ describe('Retrieve', () => {
 
   it('should find observations with a value set from included library', async function () {
     const p = await this.pharyngitisConditions.exec(this.ctx);
-    p.should.have.length(1);
+    should(p).have.length(1);
     p[0].id.should.equal('http://cqframework.org/3/2');
     this.ctx.evaluatedRecords.should.have.length(1);
     this.ctx.evaluatedRecords.should.containDeep(p);
@@ -38,7 +39,7 @@ describe('Retrieve', () => {
 
   it('should find encounter performances with a value set', async function () {
     const a = await this.ambulatoryEncounters.exec(this.ctx);
-    a.should.have.length(3);
+    should(a).have.length(3);
     a[0].id.should.equal('http://cqframework.org/3/1');
     a[1].id.should.equal('http://cqframework.org/3/3');
     a[2].id.should.equal('http://cqframework.org/3/5');
@@ -48,7 +49,7 @@ describe('Retrieve', () => {
 
   it('should find encounter performances by code', async function () {
     const e = await this.encountersByCode.exec(this.ctx);
-    e.should.have.length(3);
+    should(e).have.length(3);
     e[0].id.should.equal('http://cqframework.org/3/1');
     e[1].id.should.equal('http://cqframework.org/3/3');
     e[2].id.should.equal('http://cqframework.org/3/5');
@@ -58,19 +59,19 @@ describe('Retrieve', () => {
 
   it('should not find conditions with wrong valueset', async function () {
     const e = await this.wrongValueSet.exec(this.ctx);
-    e.should.be.empty();
+    should(e).be.empty();
     this.ctx.evaluatedRecords.should.be.empty;
   });
 
   it('should not find encounter performances using wrong codeProperty', async function () {
     const e = await this.wrongCodeProperty.exec(this.ctx);
-    e.should.be.empty();
+    should(e).be.empty();
     this.ctx.evaluatedRecords.should.be.empty;
   });
 
   it('should find conditions by specific pharyngitis code', async function () {
     const e = await this.conditionsByCode.exec(this.ctx);
-    e.should.have.length(1);
+    should(e).have.length(1);
     e[0].id.should.equal('http://cqframework.org/3/2');
     this.ctx.evaluatedRecords.should.have.length(1);
     this.ctx.evaluatedRecords.should.containDeep(e);
@@ -78,7 +79,7 @@ describe('Retrieve', () => {
 
   it('should find conditions by specific pharyngitis concept', async function () {
     const e = await this.conditionsByConcept.exec(this.ctx);
-    e.should.have.length(1);
+    should(e).have.length(1);
     e[0].id.should.equal('http://cqframework.org/3/2');
     this.ctx.evaluatedRecords.should.have.length(1);
     this.ctx.evaluatedRecords.should.containDeep(e);
